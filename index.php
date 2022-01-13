2581ea8c8f4fb047da411ff32bb549eb123f353c
Nikhil Prasad <neil1407@users.noreply.github.com>
2022-01-13 20:45:45 +0530

    Updated the php file

--- index.php
+++ index.php
@@ -46,3 +46,39 @@ $conn->close();
 }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Submitted</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <div>
+            <h2>Thank you for your valuable feedback</h2>
+            <hr>
+            <p> We sincerely appreciate your insight because it helps us build a better customer experience.
+                <br>
+                If you have any more questions, or concerns please feel welcome to reach us as we would be more than happy to
+                assist.
+            </p>
+            <button class="btn">
+                <a href="mailto: neilprasad1407@gmail.com">Contact Us</a>
+            </button>
+        </div>
+        <div class="foot">
+            <a class="social-link" href="https://www.linkedin.com/in/nikhil-kumar-prasad-87a70415a/">
+                 <i class="social fa fa-linkedin"></i></a>
+            <a class="social-link" href="https://github.com/neil1407"> 
+                <i class="social fa fa-github"></i></a>
+            <a class="social-link" href="https://www.instagram.com/neilbyahut/"> 
+                <i class="social fa fa-instagram"></i></a>
+        </div>   
+    </div>
+   
+</body>
+</html>
